56d8fe7b9d166864681f15feb98dbd3999bbfad0
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-04-24 16:16:40 +0500

    24/4/2020 4:16 pm

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,22 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
-	
-		
-	@Override
-	public void start(Stage primaryStage) throws IOException {
-		Login mainV = new Login();
-		mainV.display(); //call MainView
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setScene(new Scene(root, 804, 476));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/sample/test.java
+++ src/sample/test.java
@@ -1,135 +1,45 @@
 package sample;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-/*
-4. You are required to design a class named computer. Each computer has a companyName,
-model, color, processorType and price. A type of computer is DesktopComputer, that
-has companyName, model, color, processorType, price and
-monitorDimensions. Another type of computer is LaptopComputer that has companyName,
-model, color, processorType, price, camera, size and weight.
-a. Identify abstract and concrete classes and design the above hierarchy using appropriate
-Object-Oriented Programming principles. Also use an abstract method to displayData().
-b. Create a driver file in which
-· Create an ArrayList named computerList of size n with computer as declared type.
-· The program should then take ‘type of Computer’ as input from the user n times and in
-each iteration, instantiate index of list with entered type.
-· Call the overridden method for each index of the list
-*/
 
-abstract class computer{
-    String companyName, model, color, processorType;
-    int price;
-    Scanner scan = new Scanner(System.in);
+class person{
+    final int rollnumber;
+    public final static int staticMain = 44;
 
-
-    public abstract void TakeInput();
-    public abstract String toString();
+    person() {
+        this.rollnumber = 101;
+    }
+    public void display(final int para){
+//        para += 20; cannot do this
+        System.out.println(rollnumber+" "+para);
+    }
+    final public void add(){
+        System.out.println("methods which is final");
+    }
 }
 
-class DesktopComputer extends computer{
-    public String monitorDimensions;
-
-    public void TakeInput(){
-        System.out.print("Enter the company name of the DesktopComputer: ");
-        companyName = scan.nextLine();
-
-        System.out.print("Enter the model of the DesktopComputer: ");
-        model = scan.nextLine();
-
-        System.out.print("Enter the color of the DesktopComputer: ");
-        color = scan.nextLine();
-
-        System.out.print("Enter the processorType of the DesktopComputer: ");
-        processorType = scan.nextLine();
-
-        System.out.print("Enter the price of the DesktopComputer: ");
-        price = scan.nextInt();
-
-        System.out.print("Enter the monitorDimensions of the DesktopComputer: ");
-        monitorDimensions = scan.nextLine();
-
-        System.out.println("Now all the details are saved");
+final class teacher extends person{
+    teacher(){
+        super();
     }
+//    public void add(){
+//        // cannot override this methods
+//    }
 
-    public String toString(){
-        return(companyName+ model+ color+ processorType+price+monitorDimensions);
+    public void display(int para){
+        super.display(12);
     }
 
 }
 
-class LaptopComputer extends computer{
-    double  camera, size, weight;
-
-    public void TakeInput(){
-        System.out.print("Enter the company name of the LaptopComputer: ");
-        companyName = scan.nextLine();
-
-        System.out.print("Enter the model of the LaptopComputer: ");
-        model = scan.nextLine();
-
-        System.out.print("Enter the color of the LaptopComputer: ");
-        color = scan.nextLine();
-
-        System.out.print("Enter the processorType of the LaptopComputer: ");
-        processorType = scan.nextLine();
-
-        System.out.print("Enter the price of the LaptopComputer: ");
-        price = scan.nextInt();
-
-        System.out.print("Enter the camera of the LaptopComputer: ");
-        camera = scan.nextDouble();
-
-        System.out.print("Enter the Size of the LaptopComputer: ");
-        size = scan.nextDouble();
-
-        System.out.print("Enter the weight of the LaptopComputer: ");
-        weight = scan.nextDouble();
-
-        System.out.println("Now all the details are saved");
-    }
-
-    public String toString(){
-        return(companyName+ model+ color+ processorType+price+ camera+ size+ weight);
-    }
-}
-public class test {
+public class test{
     public static void main(String[] args) {
+        person Fahad = new person();
+        Fahad.display(13);
+        Fahad.add();
+        System.out.println(person.staticMain);
 
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter the lenght/n times of the computer Array: ");
-        int num = scan.nextInt();
-
-        ArrayList<computer> typeOfComputer = new ArrayList<computer>(num);
-        computer ref;
-        int type;
-        for (int i = 0; i < num; i++) {
-            System.out.print("Enter the type Of computer (1 => DesktopComputer,2 => LaptopComputer): ");
-            type = scan.nextInt();
-
-            switch (type) {
-                case 1:
-                    typeOfComputer.add(new DesktopComputer());
-                    ref = typeOfComputer.get(0);
-                    ref.TakeInput();
-                    break;
-
-                case 2:
-                    typeOfComputer.add(new LaptopComputer());
-                    ref = typeOfComputer.get(0);
-                    ref.TakeInput();
-                    break;
-                default:
-                    System.out.println("You Enter a wrong thing again enter the type :)");
-                    i--;
-                    break;
-            }
-        }
+        teacher t1 = new teacher();
 
 
-        for (computer member : typeOfComputer) {
-            System.out.println(member.toString());
-        }
     }
 }
