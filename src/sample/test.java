6918a94c3361db2e830fd660a3749999dbee6d6a
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-05-04 04:01:49 +0500

    4/5/2020 4:01 am

--- src/sample/test.java
+++ src/sample/test.java
@@ -1,12 +1,52 @@
 package sample;
 
-interface Car {
-    public int NoofWheel = 0;
+public class test {
+    public static void main(String[] args) {
+        MailMessage send1 = new MailMessage();
+        send1.SendMailMessage();
+    }
 }
 
+class MailMessage{
+    private Header header;
+    private Body bd;
+    private Attachment attachment;
 
-public class test{
-    public static void main(String[] args) {
+    MailMessage(){
+        header = new Header();
+        bd = new Body();
+        attachment = new Attachment();
+    }
 
+    public void SendMailMessage(){
+        System.out.println(toString());
+    }
+
+    public String toString(){
+        return (header.getTitle()+" "+bd.getMessage()+" "+attachment.getFileName());
+    }
+}
+
+class Header{
+    public String Title =   "This is the Header of the mail message";
+
+    public String getTitle() {
+        return Title;
+    }
+}
+
+class Body{
+    public String message =   "This is the Body of the mail message";
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+class Attachment{
+    public String fileName =   "This is the Attachment of the mail message";
+
+    public String getFileName() {
+        return fileName;
     }
 }
