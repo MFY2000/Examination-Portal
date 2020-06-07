25cf803bccc87e8d1c36e2648a11049618b3ea7e
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-06-07 11:45:02 +0500

    6/7/2020 11:44 am

--- src/Exam_System/StudentPlane.java
+++ src/Exam_System/StudentPlane.java
@@ -1,4 +1,24 @@
 package Exam_System;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class StudentPlane {
-}
+
+    @FXML
+    private AnchorPane ExamShowPane;
+
+    public void PopUpExxam(ActionEvent event) {
+
+    }
+
+    public void QuizDisplay(ActionEvent event) throws IOException {
+            AnchorPane pane3 = FXMLLoader.load(getClass().getResource("FXML/Dropdown.fxml"));
+            ExamShowPane.getChildren().setAll(pane3);
+        }
+
+    }
